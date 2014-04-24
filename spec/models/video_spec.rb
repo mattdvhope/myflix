46d7f9ee779a11259b544261cbf8cdbe3f969e6d
mattdvhope <mattagape@me.com>
2014-04-24 11:29:09 +0700

    Wrote comments about search in video_spec.rb

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,9 @@ describe Video do
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  # 1. Where you cannot find a single video
+  
+  # 2. Where you can find one
+  # 3. Where you can find multiple videos.
 end
