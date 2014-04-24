ff942a73d4ca08e27641e14c6e2692f11bb90f7c
mattdvhope <mattagape@me.com>
2014-04-24 11:28:07 +0700

    Prepared routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:index, :show] do
+    collection do
+      get 'search', to: 'videos#search'
+    end
+
+    member do
+      post 'highlight', to: 'videos#highlight'
+    end
+  end
+
   get '/home', to: 'videos#index'
 
   resources :categories, only: [:show]
