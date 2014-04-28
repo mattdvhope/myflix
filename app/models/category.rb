ad7cdeb33bbb98265f5bc9ef852cb4589c5425e8
mattdvhope <mattagape@me.com>
2014-04-29 06:47:22 +0700

    Implemented front page, etc & added authentication

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ActiveRecord::Base
   has_many :videos, ->{ order("created_at DESC") }
 
   def recent_videos # 6 most recent videos; most recent video should be first
-    self.videos.first(6)
+    self.videos.first(6) # don't necessarily need 'self.'
   end
 
 end
