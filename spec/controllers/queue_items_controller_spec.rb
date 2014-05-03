3bfa8714bf576c79a05aa4553b2a49c6ab0c2865
mattdvhope <mattagape@me.com>
2014-05-03 22:28:29 +0700

    Added video to my queue

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -15,4 +15,54 @@ describe QueueItemsController do
       expect(response).to redirect_to sign_in_path
     end
   end
+
+  describe "POST create" do
+    it "redirect to the my_queue_path page" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(response).to redirect_to my_queue_path
+    end
+    it "creates a queue item" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.count).to eq(1)
+    end
+    it "creates the queue item that is associated with the video" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.first.video).to eq(video)
+    end
+    it "creates the queue item that is associated with the signed in user" do
+      alice = Fabricate(:user)
+      session[:user_id] = alice.id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.first.user).to eq(alice)
+    end
+    it "puts the video as the last one in the queue" do
+      alice = Fabricate(:user)
+      session[:user_id] = alice.id
+      bugs = Fabricate(:video)
+      Fabricate(:queue_item, video: bugs, user: alice)
+      road = Fabricate(:video)
+      post :create, video_id: road.id
+      road_queue_item = QueueItem.where(video_id: road.id, user_id: alice.id).first
+      expect(road_queue_item.position).to eq(2) # 'position' is an attribute of QueueItem
+    end
+    it "does not add the video to the queue if the video is already in the queue" do
+      alice = Fabricate(:user)
+      session[:user_id] = alice.id
+      bugs = Fabricate(:video)
+      Fabricate(:queue_item, video: bugs, user: alice)
+      post :create, video_id: bugs.id
+      expect(alice.queue_items.count).to eq(1) # 'position' is an attribute of QueueItem
+    end
+    it "redirects to the sign-in page for unauthenticated users" do
+      post :create, video_id: 3
+      expect(response).to redirect_to sign_in_path
+    end
+  end
 end
