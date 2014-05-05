7f68df275ffa55fe286f3f218204465db4c52a58
mattdvhope <mattagape@me.com>
2014-05-05 11:23:03 +0700

    Added delete functionality for queue items

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Myflix::Application.routes.draw do
   end
 
   resources :categories, only: [:show]
-  resources :queue_items, only: [:create]
+  resources :queue_items, only: [:create, :destroy]
 
   get 'my_queue', to: 'queue_items#index'
 
