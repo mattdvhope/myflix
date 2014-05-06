12641229c6bf77aa7fbbb401986c04595269f745
mattdvhope <mattagape@me.com>
2014-05-06 16:50:41 +0700

    Made tests for the models, queue_item.rb and user.rb

--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -30,6 +30,32 @@ describe QueueItem do
     end
   end
 
+  describe "#rating=" do
+    it "changes the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 4
+      expect(Review.first.rating).to eq(4) # Using the Review model here is like doing a 'reload'.
+    end
+    it "clears the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user: user, video: video, rating: 2)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = nil
+      expect(Review.first.rating).to be_nil # Using the Review model here is like doing a 'reload'.
+    end
+    it "creates a review with the rating if the review is not present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, user: user, video: video)
+      queue_item.rating = 2
+      expect(Review.first.rating).to eq(2) # Using the Review model here is like doing a 'reload'.
+    end
+  end
+
   describe "#category_name" do
     it "returns the category's name of the video" do
       category = Fabricate(:category, name: "dramas")
