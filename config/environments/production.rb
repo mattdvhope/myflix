dc0a9b8fb5a7956b39a0f37f7f222aaa4ab72e8f
mattdvhope <mattagape@me.com>
2014-06-16 15:26:05 +0700

    Added mailgun to environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,12 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain,
 }
   config.action_mailer.default_url_options = { host: 'aqueous-badlands-4150.herokuapp.com/' }
 end
