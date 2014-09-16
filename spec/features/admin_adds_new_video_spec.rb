84eeeea25f40f7726327698fb39ebd005e792d95
mattdvhope <mattagape@me.com>
2014-09-16 11:54:11 +0700

    Features test for admin adds new video

--- spec/features/admin_adds_new_video_spec.rb
+++ spec/features/admin_adds_new_video_spec.rb
@@ -10,5 +10,17 @@ feature 'Admin adds new video' do
     fill_in "Title", with: "Monk"
     select "Dramas", from: "Category"
     fill_in "Description", with: "SF detective"
+    save_and_open_page
+    attach_file "Large cover", "/Users/matthewmalone/Sites/myflix/spec/support/uploads/monk_large.png"
+    attach_file "Small cover", "/Users/matthewmalone/Sites/myflix/spec/support/uploads/monk.png"
+    fill_in "Video URL", with: "http://www.example.com/my_video.mp4"
+    click_button "Add Video"
+
+    sign_out # From spec/support/macros.rb -- You can sign in with or without a user.
+    sign_in # From spec/support/macros.rb
+
+    visit video_path(Video.first) # This is the show page for a selected video.
+    expect(page).to have_selector("img[src='/uploads/video/large_cover/1/monk_large.png']")
+    expect(page).to have_selector("a[href='http://www.example.com/my_video.mp4']") # the 'Watch Now' button
   end
 end
