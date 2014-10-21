7badb65839f5c5492c435f6d35bbc587eca60ea6
mattdvhope <mattagape@me.com>
2014-10-21 12:48:16 +0700

    Extracted Stripe.api_key to config/initializers/stripe.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,23 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      handle_invitation
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-      StripeWrapper::Charge.create(
+    if @user.valid? # We have to delay the creation of a user record until after the StripeWrapper::Charge is created, b/c we only want to create the user if the charge is successful, so we'll use the '#valid?' method rather than '#save' here. We can save after we've checked whether the charge was successful.
+      # Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+      charge = StripeWrapper::Charge.create(
         :amount => 999,
         :card => params[:stripeToken],
         :description => "Sign up charge for #{@user.email}"
       )
-      flash[:success] = "Thanks for your payment."
-      AppMailer.delay.send_welcome_email(@user) # Don't need 'deliver' b/c we're using the #delay method from Sidekiq.
-      redirect_to sign_in_path
+      if charge.successful? # In the use of our stubbing out of methods in our tests for this controller, we don't yet have to actually implement the "#successful?" method for 'stripe_wrapper.rb' -- for the purpose of testing. We 'drive out' the implementation of this method before we even define it (in stripe_wrapper.rb)
+        @user.save
+        handle_invitation
+        flash[:success] = "Thanks for your payment."
+        AppMailer.delay.send_welcome_email(@user) # Don't need 'deliver' b/c we're using the #delay method from Sidekiq.
+        redirect_to sign_in_path
+      else
+        flash[:error] = charge.error_message # In the use of our stubbing out of methods in our tests for this controller, we don't yet have to actually implement the "#error_message" method for 'stripe_wrapper.rb' -- for the purpose of testing. We 'drive out' the implementation of this method before we even define it (in stripe_wrapper.rb)
+        render :new
+      end
     else
       render :new
     end
