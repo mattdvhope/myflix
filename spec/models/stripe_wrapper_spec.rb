16a428984ac16c2a9309bc0a437a7d4ae960bb4a
mattdvhope <mattagape@me.com>
2014-10-21 12:58:04 +0700

    Added test for error_message, etc

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -4,7 +4,7 @@ describe StripeWrapper do
   describe StripeWrapper::Charge do
     describe ".create" do # The convention is to use a dot for class methods; use # for instance methods
       it "makes a successful charge", :vcr do # To get this 'Stripe::Token ..etc', go to https://stripe.com/docs/api#create_card_token and copy/paste it (make sure it's set on Ruby).  Not the "EXAMPLE RESPONSE" below which is a JSON object which includes its token 'id'.
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY'] # We need the secret key for this API call.
+        # Stripe.api_key = ENV['STRIPE_SECRET_KEY'] # This is now in 'config/initializers/stripe.rb' ; With it being there, it will be loaded when Rails loads, preventing clutter to our app code & our test code.
         token = Stripe::Token.create(
           :card => {
             :number => "4242424242424242",
@@ -21,8 +21,48 @@ describe StripeWrapper do
           description: "a valid charge"
         )
 
-        expect(response.amount).to eq(999)
-        expect(response.currency).to eq('usd')
+        expect(response).to be_successful # or,...expect(response.successful?).to be_true
+        # expect(response.amount).to eq(999) # We no longer need these two 'expect's b/c we only now want to check and see if they are '#successful?'
+        # expect(response.currency).to eq('usd')
+      end
+
+      it "makes a card declined charge", :vcr do
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 10,
+            :exp_year => 2018,
+            :cvc => "314"
+          }
+        ).id # This 'id' attribute is the token that we need to get from Stripe with a successful charge.  In this case, it comes from the "EXAMPLE RESPONSE" which is a JSON object which includes its token 'id'.  From... https://stripe.com/docs/api#create_card_token
+
+        # We'll do the charge here going through our StripeWrapper...
+        response = StripeWrapper::Charge.create( # To create a test charge, go to stripe.com/docs/api/ruby#create_charge to see an example. In our case, we'll only use 'amount', 'card' and 'description' since we'll always being 'usd'.
+          amount: 999,
+          card: token,
+          description: "an invalid charge"
+        )
+
+        expect(response).not_to be_successful # or,...expect(response.successful?).to be_false
+      end
+      it "returns the error message for declined charges", :vcr do
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 10,
+            :exp_year => 2018,
+            :cvc => "314"
+          }
+        ).id # This 'id' attribute is the token that we need to get from Stripe with a successful charge.  In this case, it comes from the "EXAMPLE RESPONSE" which is a JSON object which includes its token 'id'.  From... https://stripe.com/docs/api#create_card_token
+
+        # We'll do the charge here going through our StripeWrapper...
+        response = StripeWrapper::Charge.create( # To create a test charge, go to stripe.com/docs/api/ruby#create_charge to see an example. In our case, we'll only use 'amount', 'card' and 'description' since we'll always being 'usd'.
+          amount: 999,
+          card: token,
+          description: "an invalid charge"
+        )
+
+        expect(response.error_message).to eq("Your card was declined.") # This error message is the generic one from Stripe. Before we'd rescued that error, we saw that wording in red in the console when we ran rspec.
       end
     end
   end
