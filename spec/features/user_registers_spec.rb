30427c5ba7894d5e91752c7f5916d7578f65524c
mattdvhope <mattagape@me.com>
2014-10-22 12:36:37 +0700

    Completed the rest of the scenarios

--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -1,24 +1,84 @@
 require 'spec_helper'
 
 feature 'User registers', { vcr: true, js: true } do
-  scenario "with valid user info and valid card" do
+  background do
     visit register_path
-    fill_in "Email Address", with: "matt@email.test"
-    fill_in "Password", with: "password"
-    fill_in "Full Name", with: "Matt Test"
+  end
 
-    fill_in "Credit Card Number", with: "4012888888881881"
-    fill_in "Security Code", with: "123"
-    select "10 - October", from: "date_month"
-    select "2018", from: "date_year"
-
+  scenario "with valid user info and valid card" do
+    fill_in_valid_user_info
+    fill_in_valid_card
     click_button "Sign Up"
-
     expect(page).to have_content("Thanks for your payment.")
   end
-  scenario "with valid user info and invalid card"
-  scenario "with valid user info and declined card"
-  scenario "with invalid user info and valid card"
-  scenario "with invalid user info and invalid card"
-  scenario "with invalid user info and declined card"
+
+  scenario "with valid user info and invalid card" do
+    fill_in_valid_user_info
+    fill_in_invalid_card
+    click_button "Sign Up"
+    expect(page).to have_content("This card number looks invalid") # This is the error message that Stripe sends back for an invalid card.
+  end
+
+  scenario "with valid user info and declined card" do
+    fill_in_valid_user_info
+    fill_in_declined_card
+    click_button "Sign Up"
+    expect(page).to have_content("Your card was declined")
+  end
+
+  scenario "with invalid user info and valid card" do
+    fill_in_invalid_user_info
+    fill_in_valid_card
+    click_button "Sign Up"
+    expect(page).to have_content("Invalid user information. Please check the errors below.")
+  end
+  scenario "with invalid user info and invalid card" do
+    fill_in_invalid_user_info
+    fill_in_invalid_card
+    click_button "Sign Up"
+    expect(page).to have_content("This card number looks invalid") # This is the error message that Stripe sends back for an invalid card. Same as for the second test above.
+  end
+  scenario "with invalid user info and declined card" do # Won't attempt to charge credit card.
+    fill_in_invalid_user_info
+    fill_in_declined_card
+    click_button "Sign Up"
+    expect(page).to have_content("Invalid user information. Please check the errors below.")
+  end
 end
+
+def fill_in_valid_user_info
+  fill_in "Email Address", with: "matt@email.test"
+  fill_in "Password", with: "password"
+  fill_in "Full Name", with: "Matt Test"
+end
+
+def fill_in_valid_card
+  fill_in "Credit Card Number", with: "4242424242424242"
+  fill_in "Security Code", with: "123"
+  select "10 - October", from: "date_month"
+  select "2018", from: "date_year"
+end
+
+def fill_in_invalid_card
+  fill_in "Credit Card Number", with: "777"
+  fill_in "Security Code", with: "123"
+  select "10 - October", from: "date_month"
+  select "2018", from: "date_year"
+end
+
+def fill_in_declined_card
+  fill_in "Credit Card Number", with: "4000000000000002"
+  fill_in "Security Code", with: "123"
+  select "10 - October", from: "date_month"
+  select "2018", from: "date_year"
+end
+
+def fill_in_invalid_user_info
+  fill_in "Email Address", with: "matt@email.test"
+  fill_in "Full Name", with: "Matt Test"
+end
+
+
+
+
+
