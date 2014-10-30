e6b07ec9bb0cf880c7ddc4f62c54d5de75e1ae08
mattdvhope <mattagape@me.com>
2014-10-30 13:58:49 +0700

    Added decorator folder to app

---
+++ app/decorators/video_decorator.rb
@@ -0,0 +1,10 @@
+class VideoDecorator < Draper::Decorator
+
+  # def rating_average
+  #         - if @video.rating_average > 0
+  #           = "#{@video.rating_average} / 5"
+  #         - else
+  #           "No rating yet"
+  # end
+
+end
