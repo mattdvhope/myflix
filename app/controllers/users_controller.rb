8c8fdbc7c233f14121b82ed8c66ec08fafaaaade
mattdvhope <mattagape@me.com>
2014-11-03 17:34:51 +0700

    Abstracted out user_signup logic to services

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.valid? # We have to delay the creation of a user record until after the StripeWrapper::Charge is created, b/c we only want to create the user if the charge is successful, so we'll use the '#valid?' method rather than '#save' here. We can save after we've checked whether the charge was successful.
-      # Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-      charge = StripeWrapper::Charge.create(
-        :amount => 999,
-        :card => params[:stripeToken],
-        :description => "Sign up charge for #{@user.email}"
-      )
-      if charge.successful? # In the use of our stubbing out of methods in our tests for this controller, we don't yet have to actually implement the "#successful?" method for 'stripe_wrapper.rb' -- for the purpose of testing. We 'drive out' the implementation of this method before we even define it (in stripe_wrapper.rb)
-        @user.save
-        handle_invitation
-        flash[:success] = "Thanks for your payment."
-        AppMailer.delay.send_welcome_email(@user) # Don't need 'deliver' b/c we're using the #delay method from Sidekiq.
-        redirect_to sign_in_path
-      else
-        flash[:danger] = charge.error_message # In the use of our stubbing out of methods in our tests for this controller, we don't yet have to actually implement the "#error_message" method for 'stripe_wrapper.rb' -- for the purpose of testing. We 'drive out' the implementation of this method before we even define it (in stripe_wrapper.rb)
-        render :new
-      end
+    result = UserSignup.new(@user).sign_up(params[:stripeToken], params[:invitation_token]) # This is the 'Service Object' in 'app/services/user_signup.rb'. All the complex logic of the user signing up is in that 'Service Object'. We assign it to 'result' because we want to know (in 'app/services/user_signup.rb') whether its state--its return value--is :success or :failed
+
+    if result.successful? # This 'successful?' method is in 'app/services/user_signup.rb'
+      flash[:success] = "Thanks for your payment."
+      redirect_to sign_in_path
     else
-      flash[:error] = "Invalid user information. Please check the errors below."
+      flash[:danger] = result.error_message
       render :new
     end
   end
@@ -50,15 +38,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:full_name, :email, :password)
-  end
-
-  def handle_invitation
-    if params[:invitation_token].present? # If true, then that means this user has been invited.
-      invitation = Invitation.where(token: params[:invitation_token]).first
-      @user.follow(invitation.user_who_invites) # I need the 'follow' method here, so I'll have to write this method in 'user.rb'
-      invitation.user_who_invites.follow(@user)
-      invitation.update_column(:token, nil)
-    end
   end
 
 end
