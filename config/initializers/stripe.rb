1424bb6df07e6994584d21a068165bf587d2805c
mattdvhope <mattagape@me.com>
2014-11-13 14:36:05 +0700

    Created the StripeEvent configuration

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,10 @@
 # This is used in 'models/stripe_wrapper.rb' and in "stripe_wrapper_spec.rb"
 # We need the secret key for the Stripe API call.
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+StripeEvent.configure do |events|
+  events.subscribe 'charge.succeeded' do |event|
+    # Define subscriber behavior based on the event object
+    Payment.create
+  end
+end
