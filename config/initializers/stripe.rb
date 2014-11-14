48f5b803484f22a963f3963382c1cd10118bd88b
mattdvhope <mattagape@me.com>
2014-11-14 15:04:37 +0700

    Related user to payment and added amount and reference_id

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,6 +5,7 @@ Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 StripeEvent.configure do |events|
   events.subscribe 'charge.succeeded' do |event|
     # Define subscriber behavior based on the event object
-    Payment.create
+    user = User.where(customer_token: event.data.object.customer).first # We find this from something like https://dashboard.stripe.com/test/events/evt_14yGM5LSkqll2z4UNJTiSI7n (hashes within hashes until we find 'customer', which gives us the customer token (i.e., "cus_58XQkIe8VR6QBy")). Remember that 'event' is what happens at Stripe when a customer is charged via a subscription, etc.
+    Payment.create(user: user, amount: event.data.object.amount, reference_id: event.data.object.id) # We're setting the associated user, the amount payed and the reference_id (which is the id of the 'charge'object coming back from Stripe, i.e. "ch_14yGM5LSkqll2z4U3KtyKBOu") with the Payment.
   end
 end
