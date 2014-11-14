1a7ee41a0a15d61f047e17fa89aebfdcbd1a4b1d
mattdvhope <mattagape@me.com>
2014-11-14 15:07:38 +0700

    Added more specs

--- spec/requests/create_payment_on_successful_charge_spec.rb
+++ spec/requests/create_payment_on_successful_charge_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe "Create payment on successful charge" do
   let(:event_data) do
     {
-      "id" => "evt_14yGM5LSkqll2z4UNJTiSI7n", # You must convert the JSON to ruby hashes; you must use the hash-rocket symbol if the key is a string.
+      "id" => "evt_14yGM5LSkqll2z4UNJTiSI7n", # You must convert the JSON to ruby hashes; you must use the hash-rocket symbol if the key is a string. All this data below came from https://dashboard.stripe.com/test/events/evt_14yGM5LSkqll2z4UNJTiSI7n (from the Webhook Details on the bottom of the page when I'd clicked on the 'success' arrow).
       "created" => 1415857277,
       "livemode" => false,
       "type" => "charge.succeeded",
@@ -75,8 +75,26 @@ describe "Create payment on successful charge" do
     }
   end
 
-  it "creates a payment with the webhook from stripe for charge succeeded", :vcr do
+  it "creates a payment with the webhook from stripe for charge.succeeded", :vcr do
     post "/stripe_events", event_data
     expect(Payment.count).to eq(1)
+  end
+
+  it "creates the payment associated with user", :vcr do
+    alice = Fabricate(:user, customer_token: "cus_58XQkIe8VR6QBy") # From "customer" => "cus_58XQkIe8VR6QBy" above.
+    post "/stripe_events", event_data
+    expect(Payment.first.user).to eq(alice) # We need to set the associated user with the payment in 'config/initializers/stripe.rb' by looking for
+  end
+
+  it "creates the payment with the amount", :vcr do
+    alice = Fabricate(:user, customer_token: "cus_58XQkIe8VR6QBy") # From "customer" => "cus_58XQkIe8VR6QBy" above.
+    post "/stripe_events", event_data
+    expect(Payment.first.amount).to eq(999) # We need to set the associated user with the payment in 'config/initializers/stripe.rb' by looking for
+  end
+
+  it "creates the payment with reference id", :vcr do
+    alice = Fabricate(:user, customer_token: "cus_58XQkIe8VR6QBy") # From "customer" => "cus_58XQkIe8VR6QBy" above.
+    post "/stripe_events", event_data
+    expect(Payment.first.reference_id).to eq("ch_14yGM5LSkqll2z4U3KtyKBOu") # The 'reference_id' should be the id of the 'charge' object coming back from Stripe. Above you can see that the 'object' is actually called 'charge'; immediately before it you can find... "id" => "ch_14yGM5LSkqll2z4U3KtyKBOu"
   end
 end
