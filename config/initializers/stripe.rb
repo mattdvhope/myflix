e6948452890b62a8da676a819117bf924b5a1d7b
mattdvhope <mattagape@me.com>
2014-11-15 21:19:17 +0700

    Added charge.failed for events.subscribe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,4 +8,10 @@ StripeEvent.configure do |events|
     user = User.where(customer_token: event.data.object.customer).first # We find this from something like https://dashboard.stripe.com/test/events/evt_14yGM5LSkqll2z4UNJTiSI7n (hashes within hashes until we find 'customer', which gives us the customer token (i.e., "cus_58XQkIe8VR6QBy")). Remember that 'event' is what happens at Stripe when a customer is charged via a subscription, etc.
     Payment.create(user: user, amount: event.data.object.amount, reference_id: event.data.object.id) # We're setting the associated user, the amount payed and the reference_id (which is the id of the 'charge'object coming back from Stripe, i.e. "ch_14yGM5LSkqll2z4U3KtyKBOu") with the Payment.
   end
+
+  events.subscribe 'charge.failed' do |event|
+    # Define subscriber behavior based on the event object
+    user = User.where(customer_token: event.data.object.customer).first # We find this from something like https://dashboard.stripe.com/test/events/evt_14yGM5LSkqll2z4UNJTiSI7n (hashes within hashes until we find 'customer', which gives us the customer token (i.e., "cus_58XQkIe8VR6QBy")). Remember that 'event' is what happens at Stripe when a customer is charged via a subscription, etc.
+    user.deactivate! # Need '#deactivate' method in User.rb
+  end
 end
