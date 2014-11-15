c8803c243db82d8a674ab62e931b09b068bfe53a
mattdvhope <mattagape@me.com>
2014-11-15 21:17:58 +0700

    Added conditional about active? user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,13 @@ class SessionsController < ApplicationController
   def create
     user = User.where(email: params[:email]).first
     if user && user.authenticate(params[:password]) # The 'authenticate' method is given to us by the Rails 'has_secure_password' in user.rb
-      session[:user_id] = user.id
-      redirect_to home_path, notice: 'You are signed in, enjoy!'
+      if user.active?
+        session[:user_id] = user.id
+        redirect_to home_path, notice: 'You are signed in, enjoy!'
+      else
+        flash[:error] = "Your account has been suspended, please contact customer service."
+        redirect_to sign_in_path
+      end
     else
       flash[:error] = "Invalid email or password."
       redirect_to sign_in_path
