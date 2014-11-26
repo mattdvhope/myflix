40ea649dc9e1a85524a433d82df36e7debb03da5
mattdvhope <mattagape@me.com>
2014-11-27 05:40:52 +0700

    Changed user admin default to false vs null/nil

---
+++ db/migrate/20141126222951_change_admin_user_active_default_to_false.rb
@@ -0,0 +1,5 @@
+class ChangeAdminUserActiveDefaultToFalse < ActiveRecord::Migration
+  def change
+    change_column :users, :admin, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115094914) do
+ActiveRecord::Schema.define(version: 20141126222951) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 20141115094914) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
     t.string   "customer_token"
     t.boolean  "active",          default: true
   end
