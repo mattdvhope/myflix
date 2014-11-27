9861eea9fda0f70d18276d5424d98857121e4879
mattdvhope <mattagape@me.com>
2014-11-27 20:37:45 +0700

    Added comma after ENV['S3_SECRET']

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = { # Using environment variables
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_KEY'],                # required
-      :aws_secret_access_key  => ENV['S3_SECRET']              # required
+      :aws_secret_access_key  => ENV['S3_SECRET'],              # required
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']                # required
   else
