ac514aef317dd4609b710bea22b18f1eb5d63604
mattdvhope <mattagape@me.com>
2014-11-28 18:01:26 +0700

    Added :region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,15 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage = :fog
-    config.fog_credentials = { # Using environment variables
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['S3_KEY'],                # required
-      :aws_secret_access_key  => ENV['S3_SECRET'],              # required
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      # :aws_access_key_id      => ENV['S3_KEY'],
+      # :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => 'ap-southeast-1',
     }
-    config.fog_directory  = ENV['S3_BUCKET_NAME']                # required
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
   else
     config.storage = :file
     config.enable_processing = Rails.env.development? # If we're only running tests, we'll not use mini_magick to process the images.
